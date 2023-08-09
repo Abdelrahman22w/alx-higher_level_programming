d17bc87a876994749df9b3da49f22643f5874c5c
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-09 22:23:21 +0300

    first commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for num in range(1,101):
+        if numb % 3 == 0 and num % 5 ==0:
+            print("FizzBuzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
