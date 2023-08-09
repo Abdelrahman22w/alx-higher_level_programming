744b0b4bd9f701415251c3164b7d2ae617f69606
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-09 21:42:49 +0300

    first commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(i), end='')
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
