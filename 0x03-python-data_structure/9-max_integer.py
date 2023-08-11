c3158fcbd2e963f90468fd32636203ff94200e5b
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-11 18:04:49 +0300

    first commit

---
+++ 0x03-python-data_structure/9-max_integer.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    if my_list:
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = i
+        return max
+    else:
+        return None
+     
