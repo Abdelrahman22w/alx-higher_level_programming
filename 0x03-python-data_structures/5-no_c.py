4a20ed68a39cf438551fbfdf8bbb8e42fbb6cc28
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-13 13:56:13 +0300

    first commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    for i in range (len(my_string)):
-        if i == 'c' or i == 'C':
-            my_string.pop(i)
-        return my_string
+    updated_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            updated_str += i
+    return (updated_str)
