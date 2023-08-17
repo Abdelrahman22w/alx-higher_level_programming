25374e108d5dd3ae38e489967ecdc414113b6764
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-17 21:46:50 +0300

    first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    copy = []
-    for col in matrix:
-        x = [x**2 for x in matrix]
-        copy.append(x)
-    return copy
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
