28426dbc98e0521e5645922dcb22ad84eac16149
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-17 22:01:06 +0300

    first commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
-#!usr/bin/pyshon3
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new = []
+    new_list = []
     sum = 0
     for num in my_list:
-        if num not in new:
+        if num not in new_list:
             sum += num
-            new.append(num)
-    return num
+            new_list.append(num)
+    return sum
