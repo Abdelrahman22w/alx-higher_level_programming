82470d874fcdaa66727a6e398ec735f8381f02cf
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-17 21:59:03 +0300

    first commit

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!usr/bin/pyshon3
+def uniq_add(my_list=[]):
+    sum = 0
+    new = []
+    for i in my_list:
+        if i not in new:
+            sum += i
+            new.append(i)
+    return i
