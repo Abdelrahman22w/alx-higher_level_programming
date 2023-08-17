6ab9df878ca2d7beedc3b6aca17189d506a0481c
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-08-17 22:39:18 +0300

    first commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max = 0
-    for i in a_dictionary.items():
-        if i > max:
-            max = i
-    return max
-
-    
+    maxval = 0
+    maxkey = None
+    for k, v in a_dictionary.items():
+        if v > maxval:
+            maxval = v
+            maxval = k
+    return maxkey
