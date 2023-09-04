6ab720497d89beafa8c203afb707677583b5ba50
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-09-04 20:12:26 +0300

    fdfd

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""A class defined Rectangle"""
+class Rectangle:
+	"""defined class"""
+	def __init__(self, width=0, height=0):
+		"""initializing the rectangle components
+		Args:
+			width: the rectangle width
+			height: the rectangle height
+		Raises:
+			TypeError: if size is not int
+			ValueError: if size is negative
+		"""
+		
+		self.width = width
+		self.height = height
+	@property
+	def width(self):
+		"""make width attribute"""
+		return self.__width
+	
+	@width.setter
+	def width(self, value):
+		"""set width attribute"""
+		if not isinstance(value, int):
+			raise TypeError("width must be an integer")
+		if value < 0:
+			raise ValueError("width must be >= 0")
+		self.__width = value
+		
+	@property
+	def height(self):
+		"""make height attribute"""
+		return self.__height
+	@height.setter
+	def height(self, value):
+		"""set height attribute"""
+		if not isinstance(value, int):
+			raise TypeError("height must be an integer")
+		if value < 0:
+			raise ValueError("height must be >= 0")
+		self.__height = value
+	def area(self):
+		"""rETURN THE AREA"""
+		return (self.__height * self.__width)
+	def perimeter(self):
+		"""rETURN THE perimeter"""
+		return ((self.__height * 2) + (self.__width * 2))
+		
+	
