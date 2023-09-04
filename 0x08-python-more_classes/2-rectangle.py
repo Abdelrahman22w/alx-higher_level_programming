85d8e694fb33875640403a75cef9ae2c9e6813cd
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-09-04 20:39:27 +0300

    fdfd

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-"""A class defined Rectangle"""
+"""A class that defines a rectangle"""
+
+
 class Rectangle:
-	"""defined class"""
+	"""this represents a rectangle"""
+
 	def __init__(self, width=0, height=0):
-		"""initializing the rectangle components
+		"""Initializing this rectangle class
 		Args:
-			width: the rectangle width
-			height: the rectangle height
+			width: represents the width of the rectangle
+			height: represents the height of the rectangle
 		Raises:
-			TypeError: if size is not int
-			ValueError: if size is negative
+			TypeError: if size is not integer
+			ValueError: if size is less than zero
 		"""
-		
 		self.width = width
 		self.height = height
 
 	@property
 	def width(self):
-		"""make width attribute"""
+		"""retrieves width attribute"""
 		return self.__width
-	
+
 	@width.setter
 	def width(self, value):
-		"""set width attribute"""
+		"""sets width attribute"""
 		if not isinstance(value, int):
 			raise TypeError("width must be an integer")
 		if value < 0:
 			raise ValueError("width must be >= 0")
 		self.__width = value
-		
+
 	@property
 	def height(self):
-		"""make height attribute"""
+		"""retrieves height attribute"""
 		return self.__height
 
 	@height.setter
 	def height(self, value):
-		"""set height attribute"""
+		"""sets height attribute"""
 		if not isinstance(value, int):
 			raise TypeError("height must be an integer")
 		if value < 0:
@@ -44,10 +46,11 @@ class Rectangle:
 		self.__height = value
 
 	def area(self):
-		"""Return the area of the rectangle"""
+		"""Returns the area of the rectangle"""
 		return (self.__width * self.__height)
+
 	def perimeter(self):
-		"""Return the perimeter of the rectangle"""
+		"""Returns the perimeter of the rectangle"""
+		if self.__width == 0 or self.__height == 0:
+			return (0)
 		return ((self.__width * 2) + (self.__height * 2))
-		
-	
