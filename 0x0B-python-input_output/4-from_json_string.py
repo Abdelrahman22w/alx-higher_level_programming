3ff6f6b81af84a92200001c74105ccaca50151db
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-09-12 21:51:06 +0300

    fdfd

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""This defines a JSON-to-object function"""
+
+
 import json
-"""Define a function that returns the JSON representation of an object"""
 
 
 def from_json_string(my_str):
+    """Returns the Python object representation of a JSON string"""
     return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""This defines a JSON file-writing function"""
 import json
-"""Define a function that write object"""
 
 
 def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON format"""
     with open(filename, "w") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
