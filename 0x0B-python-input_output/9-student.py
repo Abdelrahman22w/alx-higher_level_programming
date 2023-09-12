843da59ecd35951a947700661af5bc6852ad3d38
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-09-12 21:54:08 +0300

    fdfd

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Define a class od student"""
+"""This module defines a class Student"""
 
 
 class Student:
+    """class of a student"""
+
     def __init__(self, first_name, last_name, age):
         """
-        Args:
-            first_name: student's fisrt name
-            last_name: student's last name
-            age: student's age
+        Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            return self.__dict__
+    def to_json(self):
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
