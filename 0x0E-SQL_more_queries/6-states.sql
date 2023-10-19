bd2d5881341cac2e306f55e7986000e314aa3f5b
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-10-19 13:35:15 +0300

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,13 +1,12 @@
 -- Create db 'hbtn_0d_usa'
 -- If db already exists, script should not fail
-CREATE DATABASE IF NOT exists hbtn_0d_usa
+CREATE DATABASE IF NOT exists hbtn_0d_usa;
 
 -- Create table 'states' in db 'hbtn_0d_usa'
 -- id INT unique auto-generated not null and primary key
 -- name VARCHAR(256) not null
 -- If table already exists, script should not fail
 CREATE TABLE IF NOT EXIST hbtn_0d_usa.states(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
-    PRIMARY KEY (id)
 );
