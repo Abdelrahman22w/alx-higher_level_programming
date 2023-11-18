dcc22067c3b561dc67ceb51af0b3f6ac7eedb008
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-18 19:47:30 +0200

    fcommit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
-#!usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa """
-import MYSQLdb
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MYSQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = db.cursor()
     curs.execute("SELECT * FROM states")
     rows = curs.fetchall()
