4d21ed745060558d07405f9cba4be806b63f8b38
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-18 19:46:15 +0200

    fcommit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import MYSQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MYSQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!usr/bin/node
+const Int = parseInt(process.argv[2]);
+if (Number.isNaN(Int))
+{
+    console.log("Not a number");
+}
+else
+{
+    console.log("My number: " + Int);
+}
