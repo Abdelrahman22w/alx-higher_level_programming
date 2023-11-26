b49b1e1f8eba448f118dbc798bb253b198d9569c
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 20:49:23 +0200

    fsds

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
+// prints a message depending of the number of arguments passed
+
 const count = process.argv.length;
 if (count < 3)
 {
-    console.log("No argument");
+    console.log('No argument');
 }
 else if (count == 3)
 {
-    console.log("Argument found");
+    console.log('Argument found');
 }
 else
 {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
