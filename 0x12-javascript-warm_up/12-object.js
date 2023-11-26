d4faaf20d9b69798bf63542d9ed6a030731a0819
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 21:55:37 +0200

    fgs

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -3,9 +3,11 @@ const myObject = {
     type: 'object',
     value: 12
   };
+  
   console.log(myObject);
   
-// Update the value from 12 to 89
+  // Update the value from 12 to 89
   myObject.value = 89;
-
+  
   console.log(myObject);
+  
