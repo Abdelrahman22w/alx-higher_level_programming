cd4fb017959e976781896c5f90533151a840736b
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 20:55:51 +0200

    fsds

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-const Int = parseInt(process.argv[2]);
-if (Number.isNaN(Int))
-{
-    console.log("Not a number");
-}
-else
-{
-    console.log("My number: " + Int);
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
