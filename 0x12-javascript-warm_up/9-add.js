45049fc1665b9dc8dbdf6b4ed3e31689d0c187a6
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 21:11:18 +0200

    fsds

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 // prints the addition of 2 integers
 
-if (process.argv.length !== 4) {
-    console.error('Usage: ./script.js <integer1> <integer2>');
-    process.exit(1);
-}
-
 function add(a, b) {
     return parseInt(a) + parseInt(b);
 }
