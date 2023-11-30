79f89d07f4e8c4575bc1e7db84c48a76e38845f7
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-30 14:10:40 +0200

    semistandard

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  
-  console.log(myObject);
-  
-  // Update the value from 12 to 89
-  myObject.value = 89;
-  
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+// Update the value from 12 to 89
+myObject.value = 89;
+
+console.log(myObject);
