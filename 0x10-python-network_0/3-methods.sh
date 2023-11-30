28e50bb5e3f761e96124b362b7d8e401c14174ee
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-30 13:14:19 +0200

    fsd

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | grep "ALLOW: " | cut -d ' ' -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
