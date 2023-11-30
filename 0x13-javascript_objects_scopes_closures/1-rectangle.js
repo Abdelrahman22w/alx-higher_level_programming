a8d29986e4641cbb13f86695ac55a143e1b801f5
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-30 13:51:32 +0200

    done mandatory

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
     constructor (w, h) {
-      this.width = w;
-      this.height = h;
+      [this.width, this.height] = [w, h];
     }
-  }
-  
-  export default Rectangle;
+  };
