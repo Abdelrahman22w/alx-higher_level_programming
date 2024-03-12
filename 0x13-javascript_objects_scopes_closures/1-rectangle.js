8f24dbf94d3e6393bd18fe8776a9bd652cf4067f
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2024-03-12 12:50:45 +0200

    dsds

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // create class `Rectangle` that defines a rectangle
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
